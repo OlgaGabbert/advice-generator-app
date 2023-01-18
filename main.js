a4484fe9278f06b60ea6edec910771b73d57478a
Olga Gabbert <olga.gabbert@gmail.com>
2023-01-18 22:27:28 +0100

    javascript and basic styling

---
+++ main.js
@@ -0,0 +1,13 @@
+let randomAdviceID = document.getElementById('advice-id');
+let randomAdvice = document.getElementById('advice-container');
+
+async function fetchRandomAdvice() {
+    let response = await fetch("https://api.adviceslip.com/advice");
+    let advice = await response.json();
+    console.log(response);
+    let myRandomAdviceID = advice.slip.id;
+    let myRandomAdvice = advice.slip.advice;
+    console.log(myRandomAdvice);
+    randomAdvice.textContent = "\"" + myRandomAdvice + "\"";
+    randomAdviceID.textContent = myRandomAdviceID;
+}
